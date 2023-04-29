6d9b7e0a5b80eafc917b9861fbcc555a40a05ba6
paszo <paszo2008@gmail.com>
2023-04-29 21:06:08 +0200

    cart elements

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -4,6 +4,7 @@ import { GoPrimitiveDot } from 'react-icons/go';
 import {Stacked, Pie, Button, SparkLine} from '../components';
 import {earningData, SparklineAreaData, ecomPieChartData} from "../data/dummy";
 import { useStateContext} from '../contexts/ContextProvider';
+import {distinctStringValues} from "@syncfusion/ej2-react-grids";
 
 const Ecommerce = () => {
     return (
@@ -29,7 +30,33 @@ const Ecommerce = () => {
                     </div>
                 </div>
 
+                <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+                    {earningData.map((item) => (
+                        <div
+                            key={item.title}
+                            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+                        >
+                            <button type="button"
+                                style={{color: item.iconColor, backgroundColor: item.iconBg}}
+                                    className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+                            >
+                                {item.icon}
+                            </button>
+                            <p className="mt-3">
+                                <span className="text-bg font-semibold">
+                                    {item.amount}
+                                </span>
+                                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                                    {item.percentage}
+                                </span>
+                            </p>
+                            <p className="text-sm text-gray-400 mt-1">
+                                {item.title}
+                            </p>
 
+                        </div>
+                    ))}
+                </div>
 
             </div>
         </div>

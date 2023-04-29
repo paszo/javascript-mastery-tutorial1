941cb1ddeb7f090a9806073aaa47958f707924cb
paszo <paszo2008@gmail.com>
2023-04-29 20:08:48 +0200

    corrected the icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,8 +21,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
       >
-        {icon}
       </span>
+        {icon}
     </button>
   </TooltipComponent>
 );

a3b826fd539f2b52cd299ee2dd4c9fee5d8a0890
paszo <paszo2008@gmail.com>
2023-05-01 09:25:15 +0200

    callendar

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
                             {/* Apps */}
                             <Route path="/kanban" element={<Kanban/>}/>
                             <Route path="/editor" element={<Editor/>}/>
-                            <Route path="/callendar" element={<Calendar/>}/>
+                            <Route path="/calendar" element={<Calendar/>}/>
                             <Route path="/color-picker" element={<ColorPicker/>}/>
                             {/* Charts */}
                             <Route path="/line" element={<Line/>}/>
